b3d3fdf0e50839ba59d5357bf621bd71a2a40752
yacin74935-byte <yacin74935@gmail.com>
2026-09-23 15:12:16 +0100

    Update pair.js

--- scripts/cmds/pair.js
+++ scripts/cmds/pair.js
@@ -9,7 +9,7 @@ const baseApiUrl = async () => {
 
 module.exports = {
         config: {
-                name: "pair",
+                name: "تزويج",
                 version: "1.7",
                 author: "MahMUD",
                 countDown: 10,
